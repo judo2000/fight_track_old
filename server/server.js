caa415934a68481d27f47986ab8e814f0a44b223
Stephen Moore <judo2000@comcast.net>
2023-10-11 14:17:08 -0600

    Update Server for redeploy

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use("/api/users", userRoutes);
 
 const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, "client", "dist", "index.html")));
+app.use(express.static(path.join(__dirname, "/client/dist")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "/client/dist/index.html"));
